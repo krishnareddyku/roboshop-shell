2057027dfa2853746720e2f108f4e4aeaef10a2b
krishnareddyku <krishnareddyku@gmail.com>
2023-01-10 20:30:38 +0530

    mongodb.v2.sh updated

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,5 @@
-cp files/mongodb.repo /etc/yum.repos.d/mongo.repo
+files="$(pwd)/files"
+cp $files/mongodb.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
