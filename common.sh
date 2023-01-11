1616f199e18be872f2841a8f248e454b8dffa4b0
krishnareddyku <krishnareddyku@gmail.com>
2023-01-11 16:39:44 +0530

    script improvements added

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 script_location="$(pwd)/files"
 LOG=/tmp/robohop.log
-
+exec &>>${LOG}
 status_check () {
   if [ $? -eq 0 ];then
     echo -e "\e31mSUCCESS\e[0m"
--- frontend.sh
+++ frontend.sh
@@ -1,5 +1,4 @@
 source common.sh
-exec &>>${LOG}
 
 echo -e "\e[35m Install Nginx\e[0m"
 yum install nginx -y
