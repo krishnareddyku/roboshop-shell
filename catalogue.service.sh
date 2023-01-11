09103ebb9d5fda02aa58aaf0351cb80d73612dd5
krishnareddyku <krishnareddyku@gmail.com>
2023-01-11 16:00:30 +0530

    frontend.sh script improvements added

--- catalogue.service.sh
+++ catalogue.service.sh
@@ -1,4 +1,7 @@
+files="$(pwd)/files"
+echo -e "\e[35m execute nodejs repo\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[35m Install nodejs\e[0m"
 yum install nodejs -y
 useradd roboshop
 mkdir /app
@@ -9,6 +12,6 @@ npm install
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
-cp files/mongo.repo /etc/yum.repos.d/mongo.repo
+cp $files/mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org-shell -y
 mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,43 @@
 files="$(pwd)/files"
+LOG=/tmp/robohop.log
+exec &>>${LOG}
+
+status (){
+  if [ $? -eq 0 ];then
+    echo SUCESS
+    else
+    echo FAILURE
+    exit
+  fi
+}
+
 echo -e "\e[35m Install Nginx\e[0m"
 yum install nginx -y
+status
+
 echo -e "\e[35m Remove Nginx Old Content\e[0m"
 rm -rf /usr/share/nginx/html/*
+status
+
 echo -e "\e[35m Download Frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+status
+
 cd /usr/share/nginx/html
+status
+
 echo -e "\e[35m Extract Frontend Content\e[0m"
 unzip /tmp/frontend.zip
+status
+
 echo -e "\e[35m update nginx config\e[0m"
 cp ${files}/nginx.roboshop.conf /etc/nginx/default.d/roboshop.conf
+status
+
 echo -e "\e[35m Restart Nginx\e[0m"
 systemctl restart nginx
+status
+
 echo -e "\e[35m Enable systemd nginx\e[0m"
 systemctl enable nginx
+status
