ae4f26e65a5738b44038cc324d8d81a756468cc8
krishnareddyku <krishnareddyku@gmail.com>
2023-01-11 16:48:00 +0530

    script improvements added

--- common.sh
+++ common.sh
@@ -3,9 +3,9 @@ LOG=/tmp/robohop.log
 exec &>>${LOG}
 status_check () {
   if [ $? -eq 0 ];then
-    echo -e "\e31mSUCCESS\e[0m"
+    echo -e "\e[1;31mSUCCESS\e[0m"
     else
-    echo -e "\e31mFAILURE\e[0m"
+    echo -e "\e[1;31mFAILURE\e[0m"
     echo "refer ${LOG} more info"
     exit
   fi
